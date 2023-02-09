63710743f5cc13f8068aa81aab923d6eaa8d99d4
mudlord <mudlord@protonmail.com>
2023-02-10 07:30:00 +1100

    Use proper VFS version

--- libretro/libretro.c
+++ libretro/libretro.c
@@ -1186,7 +1186,7 @@ void retro_set_environment(retro_environment_t cb)
    libretro_set_core_options(environ_cb, &option_cats_supported);
 
    /* Initialise VFS */
-   vfs_iface_info.required_interface_version = 1;
+   vfs_iface_info.required_interface_version = 2;
    vfs_iface_info.iface                      = NULL;
    if (environ_cb(RETRO_ENVIRONMENT_GET_VFS_INTERFACE, &vfs_iface_info))
       filestream_vfs_init(&vfs_iface_info);
